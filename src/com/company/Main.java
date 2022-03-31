caaf3f630bcafb32634175e943b766c69c87cc2f
Sona <sonochka.velieva@yandex.ru>
2022-03-31 23:28:55 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+        long countYoungPeople = persons.stream()
+                .filter(x -> x.getAge() < 18)
+                .count();
+        System.out.println(countYoungPeople);
+
+
+        List<String> recruitFamilies = persons.stream()
+                .filter(x -> x.getSex() == Sex.MAN)
+                .filter(x -> x.getAge() > 18 && x.getAge() < 27)
+                .map(x -> x.getFamily())
+                .collect(Collectors.toList());
+        System.out.println(recruitFamilies);
+
+
+        List<String> workersWithHigherEducation = persons.stream()
+                .filter(x -> x.getSex() == Sex.WOMAN && x.getAge() > 18 && x.getAge() < 60
+                    || x.getSex() == Sex.MAN && x.getAge() > 18 && x.getAge() < 65)
+                .filter(x -> x.getEducation() == Education.HIGHER)
+                .sorted(Comparator.comparing(x -> x.getFamily()))
+                .map(x -> x.getFamily())
+                .collect(Collectors.toList());
+        System.out.println(workersWithHigherEducation);
+    }
+}
